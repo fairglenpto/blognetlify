296560cc02e01ba699c75da444f20b0fcdaf95f0
Shea-Nelson <shea.nelson@icloud.com>
2018-03-20 10:04:40 -0400

    'updated_title'

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,7 +8,7 @@ import './index.css'
 const TemplateWrapper = ({ children, data, location }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Fairglen PTO"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ export default class About extends Component {
             <div>
                 <h1>About Us</h1>
                 <p>
-                    Next level biodiesel succulents hella blog lyft pour-over waistcoat. Letterpress franzen forage swag venmo vice. Roof party tbh locavore vegan four dollar toast adaptogen try-hard kickstarter. Unicorn shaman fixie adaptogen. Tousled before they sold out mlkshk, post-ironic roof party blue bottle portland vinyl hella 8-bit vexillologist hashtag small batch hell of.  
+                    About us body
                 </p>
                 </div>
         )
